564fec763c58bb692d9707422d22d75263cc1267
mrthomas20121 <thomas77300@gmail.com>
2020-06-10 18:51:53 +0200

    bumped version to 1.3.3

--- src/main/java/com/mrthomas20121/tfc_rocksplus/TFC_RocksPlus.java
+++ src/main/java/com/mrthomas20121/tfc_rocksplus/TFC_RocksPlus.java
@@ -14,7 +14,7 @@ public class TFC_RocksPlus
 {
     public static final String MODID = "tfc_rocksplus";
     public static final String NAME = "TFC Rocks+";
-    public static final String VERSION = "1.3.2";
+    public static final String VERSION = "1.3.3";
 
     private static Logger logger;
 
