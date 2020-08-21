9f7d185e3b7dbe2022e11994eea3fed25f7fdcc2
Ahmad Bassam El Bizri <34858431+ATeamInc@users.noreply.github.com>
2020-08-21 15:52:07 -0700

    Bump ver to 1.3.4

--- src/main/java/com/mrthomas20121/tfc_rocksplus/TFC_RocksPlus.java
+++ src/main/java/com/mrthomas20121/tfc_rocksplus/TFC_RocksPlus.java
@@ -14,7 +14,7 @@ public class TFC_RocksPlus
 {
     public static final String MODID = "tfc_rocksplus";
     public static final String NAME = "TFC Rocks+";
-    public static final String VERSION = "1.3.3";
+    public static final String VERSION = "1.3.4";
 
     private static Logger logger;
 
